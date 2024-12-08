98b25adb1d53fb1a274caca753815f8789d12fe5
Tiago Angelo Menegaz <tmenegaz77@gmail.com>
2024-12-07 23:13:00 -0300

    #issue-2724899778 - criar as tabelas das entidades

--- site/src/main/java/com/grimpa/site/domain/Cliente.java
+++ site/src/main/java/com/grimpa/site/domain/Cliente.java
@@ -1,18 +1,29 @@
 package com.grimpa.site.domain;
 
+import com.grimpa.site.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serial;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Cliente extends Pessoa {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
+    @OneToMany(mappedBy = "cliente")
     private List<Processo> processos = new ArrayList<>();
 
     public Cliente() {
         super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 
     public List<Processo> getProcessos() {
--- site/src/main/java/com/grimpa/site/domain/Tecnico.java
+++ site/src/main/java/com/grimpa/site/domain/Tecnico.java
@@ -1,18 +1,30 @@
 package com.grimpa.site.domain;
 
+import com.grimpa.site.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+
+import java.io.Serial;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Tecnico extends Pessoa {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "cliente")
     private List<Processo> processos = new ArrayList<>();
 
 
     public Tecnico() {
         super();
+        addPerfil(Perfil.TECNICO);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.TECNICO);
     }
 
     public List<Processo> getProcessos() {
