ac1e84d65818929b7d3c4cf267824d0f949ed7be
Tiago Angelo Menegaz <tmenegaz77@gmail.com>
2024-12-09 22:47:05 -0300

    Cliente crud

--- site/src/main/java/com/grimpa/site/domain/Cliente.java
+++ site/src/main/java/com/grimpa/site/domain/Cliente.java
@@ -2,6 +2,7 @@ package com.grimpa.site.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.grimpa.site.domain.dtos.ClienteDto;
 import com.grimpa.site.domain.enums.Perfil;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -9,6 +10,7 @@ import jakarta.persistence.OneToMany;
 import java.io.Serial;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
 public class Cliente extends Pessoa {
@@ -28,6 +30,17 @@ public class Cliente extends Pessoa {
     public Cliente(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
         addPerfil(Perfil.CLIENTE);
+    }
+
+    public Cliente(ClienteDto clienteDto) {
+        super();
+        this.id = clienteDto.getId();
+        this.nome = clienteDto.getNome();
+        this.cpf = clienteDto.getCpf();
+        this.email = clienteDto.getEmail();
+        this.senha = clienteDto.getSenha();
+        this.perfis = clienteDto.getPerfis().stream().map(Perfil::getCodigo).collect(Collectors.toSet());
+        this.dataCriacao = clienteDto.getDataCriacao();
     }
 
     public List<Processo> getProcessos() {
---
+++ site/src/main/java/com/grimpa/site/services/ClienteService.java
@@ -0,0 +1,72 @@
+package com.grimpa.site.services;
+
+import com.grimpa.site.domain.Pessoa;
+import com.grimpa.site.domain.Cliente;
+import com.grimpa.site.domain.dtos.ClienteDto;
+import com.grimpa.site.repositories.PessoaRepository;
+import com.grimpa.site.repositories.ClienteRepository;
+import com.grimpa.site.services.exceptions.DataIntegrityViolationException;
+import com.grimpa.site.services.exceptions.ObjectNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository repository;
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    public Cliente create(ClienteDto clienteDto) {
+        clienteDto.setId(null);
+        validaByCpfAndEmail(clienteDto);
+        Cliente cliente = new Cliente(clienteDto);
+        return repository.save(cliente);
+    }
+
+    @Transactional
+    public Cliente update(Integer id, ClienteDto clienteDto) {
+        clienteDto.setId(id);
+        Cliente clienteOld = this.findById(id);
+
+        validaByCpfAndEmail(clienteDto);
+        clienteOld = new Cliente(clienteDto);
+        return repository.save(clienteOld);
+    }
+
+    public List<Cliente> findAll() {
+        List<Cliente> clientes = repository.findAll();
+        return clientes;
+    }
+
+    public Cliente findById(Integer id) {
+        Optional<Cliente> cliente = repository.findById(id);
+        return cliente.orElseThrow(() -> new ObjectNotFoundException("Cliente não encontrado"));
+    }
+
+    private void validaByCpfAndEmail(ClienteDto clienteDto) {
+        Optional<Pessoa> pessoa = pessoaRepository.findByCpf(clienteDto.getCpf());
+        if (pessoa.isPresent() && pessoa.get().getId() != clienteDto.getId()) {
+            throw new DataIntegrityViolationException("Esse CPF já está cadastrado");
+        }
+
+        pessoa = pessoaRepository.findByEmail(clienteDto.getEmail());
+        if (pessoa.isPresent() && pessoa.get().getId() != clienteDto.getId()) {
+            throw new DataIntegrityViolationException("Esse e-mail já está cadastrado");
+        }
+    }
+
+    public void delete(Integer id) {
+        Cliente cliente = this.findById(id);
+        if (!cliente.getProcessos().isEmpty()) {
+            throw new DataIntegrityViolationException("Cliente possui ordem de serviço e não pode ser deletado");
+        }
+        repository.deleteById(id);
+    }
+}
