e8ef3e40acda766c7c369189863de6bfdfb09222
Tiago Angelo Menegaz <tmenegaz77@gmail.com>
2024-12-10 12:41:59 -0300

    Squashed commit of the following:
    commit ac1e84d65818929b7d3c4cf267824d0f949ed7be
    Author: Tiago Angelo Menegaz <tmenegaz77@gmail.com>
    Date:   Mon Dec 9 22:47:05 2024 -0300
    
        Cliente crud

---
+++ site/src/main/java/com/grimpa/site/domain/dtos/ClienteDto.java
@@ -0,0 +1,112 @@
+package com.grimpa.site.domain.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.grimpa.site.domain.Cliente;
+import com.grimpa.site.domain.enums.Perfil;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.br.CPF;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ClienteDto implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    protected Integer id;
+
+    @NotNull(message = "O campo NOME é requerido")
+    protected String nome;
+
+    @NotNull(message = "O campo CPF é requerido")
+    @CPF
+    protected String cpf;
+
+    @NotNull(message = "O campo E-MAIL é requerido")
+    @Email
+    protected String email;
+
+    @NotNull(message = "O campo SENHA é requerido")
+    protected String senha;
+
+    protected Set<Integer> perfis = new HashSet<>();
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
+    protected LocalDate dataCriacao = LocalDate.now();
+
+    public ClienteDto() {
+        super();
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public ClienteDto(Cliente cliente) {
+        super();
+        this.id = cliente.getId();
+        this.nome = cliente.getNome();
+        this.cpf = cliente.getCpf();
+        this.email = cliente.getEmail();
+        this.senha = cliente.getSenha();
+        this.perfis = cliente.getPerfis().stream().map(Perfil::getCodigo).collect(Collectors.toSet());
+        this.dataCriacao = cliente.getDataCriacao();
+        addPerfil(Perfil.CLIENTE);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public Set<Perfil> getPerfis() {
+        return perfis.stream().map(Perfil::toEnum).collect(Collectors.toSet());
+    }
+
+    public void addPerfil(Perfil perfil) {
+        this.perfis.add(perfil.getCodigo());
+    }
+
+    public LocalDate getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDate dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+}
---
+++ site/src/main/java/com/grimpa/site/resources/ClienteResource.java
@@ -0,0 +1,53 @@
+package com.grimpa.site.resources;
+
+import com.grimpa.site.domain.Cliente;
+import com.grimpa.site.domain.dtos.ClienteDto;
+import com.grimpa.site.services.ClienteService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "clientes")
+public class ClienteResource {
+
+    @Autowired
+    private ClienteService service;
+
+    @PostMapping
+    public ResponseEntity<ClienteDto> create(@Valid @RequestBody ClienteDto tecnicoDto) {
+        Cliente tecnico = service.create(tecnicoDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(tecnico.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ClienteDto> update(@PathVariable Integer id, @RequestBody ClienteDto tecnicoDto ) {
+        Cliente tecnico = service.update(id, tecnicoDto);
+        return ResponseEntity.ok().body(new ClienteDto(tecnico));
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClienteDto> findById(@PathVariable Integer id) {
+        Cliente tecnico = service.findById(id);
+        return ResponseEntity.ok().body(new ClienteDto(tecnico));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ClienteDto>> findAll() {
+        List<Cliente> cliente = service.findAll();
+        return ResponseEntity.ok().body(cliente.stream().map(ClienteDto::new).collect(Collectors.toList()));
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<ClienteDto> delete(@PathVariable Integer id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
